5c3c0e9bde2ce94fa092cef8376eb237434b7350
Rafał Matusiak <85128542+RafMat159@users.noreply.github.com>
2024-11-19 18:39:46 +0100

    database connection, eureka + spring cloud - load balancing (#5)

--- editorial/src/main/java/com/editorial/EditorialApplication.java
+++ editorial/src/main/java/com/editorial/EditorialApplication.java
@@ -1,11 +1,24 @@
 package com.editorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class EditorialApplication {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
+    @GetMapping("/editorial")
+    public String idTest(){
+        return "Editorial app of id: " + instanceId;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EditorialApplication.class, args);
