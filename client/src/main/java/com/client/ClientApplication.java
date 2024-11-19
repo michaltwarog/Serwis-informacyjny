eaaa8880678b0dff66e1f977978e510aeee2070f
Rafał Matusiak <rmatusiak159@gmail.com>
2024-11-19 18:38:38 +0100

    database connection, eureka + spring cloud - load balancing

--- client/src/main/java/com/client/ClientApplication.java
+++ client/src/main/java/com/client/ClientApplication.java
@@ -1,11 +1,29 @@
 package com.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-
+import java.util.List;
+import java.util.stream.Collectors;
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class ClientApplication {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
+    @GetMapping("/client")
+    public String idTest(){
+        return "Client app of id: " + instanceId;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
